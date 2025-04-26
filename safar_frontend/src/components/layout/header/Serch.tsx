ddfecce6fc4637d196bcd857a87ad5ec4e062c18
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-04-26 21:59:26 +0300

    fix Search

--- safar_frontend/src/components/layout/header/Serch.tsx
+++ safar_frontend/src/components/layout/header/Serch.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 export function MSearch() {
   return (
-        <div className="relative mx-auto max-w-4xl px-2">
+        <div className="relative mx-auto max-w-4xl">
           <div className="flex items-center rounded-full bg-card shadow-lg">
             <div className="flex-1 px-6 py-3">
               <div className="text-sm font-medium">Where</div>
@@ -13,7 +13,7 @@ export function MSearch() {
                 className="w-full border-none p-0 text-sm focus:outline-none focus:ring-0"
               />
             </div>
-            <Button className="absolute right-0 flex h-12 w-12 items-center justify-center rounded-full ">
+            <Button className="absolute right-1 flex h-12 w-12 items-center justify-center rounded-full ">
               <Search className="h-5 w-5" />
             </Button>
           </div>
