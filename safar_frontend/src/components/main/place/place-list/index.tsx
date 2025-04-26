93d03d79b1cd349d439b2596c332aac20770426b
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-04-26 23:36:57 +0300

    fix

--- safar_frontend/src/components/main/place/place-list/index.tsx
+++ safar_frontend/src/components/main/place/place-list/index.tsx
@@ -45,7 +45,7 @@ export const ListPlaces = ({ selected }: Props) => {
 
   return (
     <div className="flex flex-col w-full mt-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 w-full pb-4">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-6 w-full overflow-x-auto pb-4">
       {allFetchedPlaces.length > 0 ? (
           allFetchedPlaces.map((place) => (
              <div
