024ca833ac6fe6887365f7bdaaf6cfb136b7ec4f
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-05-04 17:01:12 +0300

    fix

--- safar_frontend/src/core/hooks/use-social-auth.ts
+++ safar_frontend/src/core/hooks/use-social-auth.ts
@@ -1,3 +1,5 @@
+"use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 import { toast } from 'sonner';
