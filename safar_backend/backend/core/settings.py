6fd07a109c7c3dcadc6fc19a085e19b6bae8861d
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-05-06 00:35:58 +0300

    fix

--- safar_backend/backend/core/settings.py
+++ safar_backend/backend/core/settings.py
@@ -252,7 +252,6 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
-        "rest_framework.throttling.UserRateThrottle"
     ],
     'DEFAULT_THROTTLE_CLASSES': [],
     "DEFAULT_THROTTLE_RATES": {
