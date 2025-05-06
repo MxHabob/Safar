290272f158c24189bff2810302a1d5ff52fc26bf
obaidallah <obaidalqershi1@gmail.com>
2025-05-06 09:35:39 +0300

    comment out signals import in SaferConfig

--- safar_backend/backend/apps/safar/apps.py
+++ safar_backend/backend/apps/safar/apps.py
@@ -5,5 +5,5 @@ class SaferConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.safar'
 
-    def ready(self):
-        import apps.safar.signals
+    # def ready(self):
+    #     import apps.safar.signals
