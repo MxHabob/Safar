c46f8d7a803d713833b316c437f91ac5a54b3715
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-05-07 22:54:41 +0300

    fix

--- safar_frontend/src/app/(main)/profile/[id]/more/page.tsx
+++ safar_frontend/src/app/(main)/profile/[id]/more/page.tsx
@@ -1,4 +1,4 @@
-import MoreProfile from "@/components/main/profile/more-profile"
+import { MoreProfile } from "@/components/main/profile/more-profile";
 
 type Props = {
 	params: Promise<{ id: string}>
@@ -6,10 +6,10 @@ type Props = {
 
 export default async  function ProfileMore({  params  }: Props) {
   const id = (await params).id
+
   return (
     <main className="min-h-full flex items-center justify-center p-4 md:p-8">
       <MoreProfile userId={id} />
-        
     </main>
   )
 }
