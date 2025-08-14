2d8ad781f01f5d8f4fdeea13ec6d92c983cd52e8
Mohamed Alhabob <110397949+MxHabob@users.noreply.github.com>
2025-08-14 16:48:18 +0300

    Edite init.sql

--- safar_backend/infrastructure/postgis/init.sql
+++ safar_backend/infrastructure/postgis/init.sql
@@ -1,35 +1,35 @@
 -- Check if the database exists and create it if not
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = current_setting('POSTGRES_DB')) THEN
-        EXECUTE format('CREATE DATABASE %I', current_setting('POSTGRES_DB'));
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = current_setting('PGDATABASE')) THEN
+        EXECUTE format('CREATE DATABASE %I', current_setting('PGDATABASE'));
     END IF;
 END $$;
 
 -- Check if the user exists and create it if not
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = current_setting('POSTGRES_USER')) THEN
-        EXECUTE format('CREATE USER %I WITH PASSWORD %L', current_setting('POSTGRES_USER'), current_setting('POSTGRES_PASSWORD'));
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = current_setting('PGUSER')) THEN
+        EXECUTE format('CREATE USER %I WITH PASSWORD %L', current_setting('PGUSER'), current_setting('PGPASSWORD'));
     END IF;
 END $$;
 
 -- Set role settings for the user
 DO $$
 BEGIN
-    EXECUTE format('ALTER ROLE %I SET client_encoding TO ''utf8''', current_setting('POSTGRES_USER'));
-    EXECUTE format('ALTER ROLE %I SET default_transaction_isolation TO ''read committed''', current_setting('POSTGRES_USER'));
-    EXECUTE format('ALTER ROLE %I SET timezone TO ''UTC''', current_setting('POSTGRES_USER'));
+    EXECUTE format('ALTER ROLE %I SET client_encoding TO ''utf8''', current_setting('PGUSER'));
+    EXECUTE format('ALTER ROLE %I SET default_transaction_isolation TO ''read committed''', current_setting('PGUSER'));
+    EXECUTE format('ALTER ROLE %I SET timezone TO ''UTC''', current_setting('PGUSER'));
 END $$;
 
 -- Grant all privileges on the database to the user
 DO $$
 BEGIN
-    EXECUTE format('GRANT ALL PRIVILEGES ON DATABASE %I TO %I', current_setting('POSTGRES_DB'), current_setting('POSTGRES_USER'));
+    EXECUTE format('GRANT ALL PRIVILEGES ON DATABASE %I TO %I', current_setting('PGDATABASE'), current_setting('PGUSER'));
 END $$;
 
 -- Connect to the database and enable PostGIS extension
-\c ${POSTGRES_DB}
+\c ${PGDATABASE}
 
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS postgis_topology;
