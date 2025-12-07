c2dac8151a6e49685828b20bb3380639bc2b850b
MxHabob <alhapopy@gmail.com>
2025-12-07 13:45:25 +0300

    Remove About and Blog page components along with their associated loading, error, and not-found states. This cleanup eliminates unused files and simplifies the project structure, enhancing maintainability.

--- web/src/app/(home)/blog/[id]/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { BlogSlugViewLoadingStatus } from "@/pages/blog/blog-slug-view";
-
-export default function Loading() {
-  return <BlogSlugViewLoadingStatus />;
-}
-
--- web/src/app/(home)/blog/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { TravelGuidesViewLoadingStatus } from "@/pages/blog/blog-view";
-
-export default function Loading() {
-  return <TravelGuidesViewLoadingStatus />;
-}
-
